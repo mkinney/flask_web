3bcd8a6207fca609d4c1c02ca47fb71c1f275d47
Mike Kinney <mike.kinney@gmail.com>
2018-11-11 15:50:57 -0800

    change to using requests for test

--- test_app.py
+++ test_app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """ Simple test to verify that app.py works as expected."""
 
-def test_app_hello(host):
+import requests
+
+def test_app_hello():
     """ See if we can hit the endpoint and get the expected response."""
-    command = host.command("curl http://localhost:5000")
-    assert command.stdout.rstrip() == "Hey, we have Flask in a Docker container!"
-    assert command.rc == 0
+    response = requests.get("http://localhost:5000")
+    assert response.status_code == 200
+    assert response.text.rstrip() == "Hey, we have Flask in a Docker container!"
